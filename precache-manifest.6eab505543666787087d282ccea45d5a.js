db895d6c866cc8b28020c411c3ad779881c01859
defauth98 <neto.daniribeiro@gmail.com>
2021-07-21 17:02:32 -0300

    Updates

---
+++ precache-manifest.6eab505543666787087d282ccea45d5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b036d662bd68d97214cd4bb05f2535",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "8a7241070259ebe87e51",
+    "url": "/trivia/static/css/main.645746b5.chunk.css"
+  },
+  {
+    "revision": "0fc88711794807feb9ea",
+    "url": "/trivia/static/js/2.656dbc86.chunk.js"
+  },
+  {
+    "revision": "26fba7765282c1c534fc63dce6295e30",
+    "url": "/trivia/static/js/2.656dbc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7241070259ebe87e51",
+    "url": "/trivia/static/js/main.05fc90a3.chunk.js"
+  },
+  {
+    "revision": "ef294555bde0349958cc",
+    "url": "/trivia/static/js/runtime-main.36b073f9.js"
+  },
+  {
+    "revision": "ef0d030f2de577545a9990994cc9d5e4",
+    "url": "/trivia/static/media/Audioslave.ef0d030f.ogg"
+  },
+  {
+    "revision": "1758016938f610a3f650cfabd3dd73c3",
+    "url": "/trivia/static/media/showDoMilhao.17580169.ogg"
+  },
+  {
+    "revision": "159c81885cfdc70e5ed79d0a5af36330",
+    "url": "/trivia/static/media/showLogo.159c8188.png"
+  }
+]);
